dbd9f9673bd0b83f9c42f624ec75f371850f5d6d
maksim <mr.rytsarev@mail.ru>
2017-10-27 09:27:29 +0300

    # The class LinkedList is establish to base functionality...

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,28 +1,7 @@
 package com.company;
-// двунаправленный динамический список хранит значение типа INT
-//
-public class LinkedList {
-    private int cursor = 0;
-    private int size = 0;
 
-// logical methods
-    public boolean hasNext() {
+class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome!");
     }
-    public boolean hasPrevious() {
-    }
-
-// navigation List methods
-    public int next() {
-    }
-
-    public int previous() {
-    }
-// processing List methods
-    public boolean add() {
-
-    }
-
-    public boolean remove() {
-    }
-
 }
