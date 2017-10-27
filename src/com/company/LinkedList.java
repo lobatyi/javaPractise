77fd89671850bf70887cf9ff17d67e1a99e19262
maksim <mr.rytsarev@mail.ru>
2017-10-27 09:03:19 +0300

    the first commit

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,28 @@
+package com.company;
+// двунаправленный динамический список хранит значение типа INT
+//
+public class LinkedList {
+    private int cursor = 0;
+    private int size = 0;
+
+// logical methods
+    public boolean hasNext() {
+    }
+    public boolean hasPrevious() {
+    }
+
+// navigation List methods
+    public int next() {
+    }
+
+    public int previous() {
+    }
+// processing List methods
+    public boolean add() {
+
+    }
+
+    public boolean remove() {
+    }
+
+}
